67fe31b7f59d3dfba825df640914e2d6c13c7391
Sara Lüthi <sara.luethi@students.bfh.ch>
2022-11-21 18:06:54 +0100

    add comparator-function and sortingKey in AdminResourceSelectField

--- src/components/field/admin-resource-select-field.jsx
+++ src/components/field/admin-resource-select-field.jsx
@@ -4,9 +4,10 @@ import { MenuItem } from '@mui/material';
 import AdminSelectField from './admin-select-field';
 import DataContext from '../crud/context/data.context';
 import ServiceContext from '../crud/context/service.context';
+import comparator from '../comparator';
 
 export default function AdminResourceSelectField({
-  name, label, resource, itemReferenceGetter, value, setValue,
+  name, label, resource, itemReferenceGetter, value, setValue, sortingKey,
 }) {
   const { dataService } = useContext(ServiceContext);
   const [data] = useContext(DataContext);
@@ -24,6 +25,7 @@ export default function AdminResourceSelectField({
     : (v) => <MenuItem key={v.id} value={v.id}>{v.name}</MenuItem>;
 
   if (values) {
+    values.sort(comparator(sortingKey));
     return (
       <AdminSelectField
         name={name}
