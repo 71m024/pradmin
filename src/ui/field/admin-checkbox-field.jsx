88a150a44846ea325d4e3e73ffec2374f620433e
Sara Lüthi <sara.luethi@students.bfh.ch>
2023-03-13 08:48:03 +0100

    rename onChange

--- src/ui/field/admin-checkbox-field.jsx
+++ src/ui/field/admin-checkbox-field.jsx
@@ -7,13 +7,13 @@ import DataContext from '../../context/data.context';
 
 export default function AdminCheckboxField({ name, label }) {
   const [data, setData] = useContext(DataContext);
-  const onChangeAutoMapping = (event) => {
+  const onChange = (event) => {
     setData({ ...data, [name]: event.target.checked });
   };
 
   return (
     <Stack direction="row" width="100%" flexWrap="wrap">
-      <Checkbox checked={data[name]} onChange={onChangeAutoMapping} inputProps={{ 'aria-label': 'controlled' }} />
+      <Checkbox checked={data[name]} onChange={onChange} inputProps={{ 'aria-label': 'controlled' }} />
       <p>{label}</p>
     </Stack>
   );
